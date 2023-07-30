74d942e3ce095d52ab45ef7670e930c3039c23f1
Danil Sherbakov <dandiaborn@gmail.com>
2023-07-30 18:03:48 +0300

    add loading

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 
 const List = (props) => {
   const [list, setList] = useState([]);
+  const [isListLoading, setIsListLoading] = useState(false);
 
   useEffect(() => {
+    setIsListLoading(true);
     axios.get(`https://swl-manager.onrender.com/${props.faction}/${props.item}`)
       .then((response) => {
         try {
@@ -13,13 +15,14 @@ const List = (props) => {
         catch (e) {
           console.log('server dont ansver');
         }
-      });
+      })
+      .finally(() => { setIsListLoading(false) });
   }, [props.faction, props.item]);
 
   return <div className="">
     <div className="list">
       <div className="list__wrapper">
-        {props.item === 'Units' ? <>
+        {props.item === 'Units' && isListLoading === false ? <>
           <div className="list__inner">
             <div className="list__exp-num">
               №
@@ -46,67 +49,87 @@ const List = (props) => {
               </div>
             </div>
           )} </>
-          : props.item === 'CommandCards' ? <>
-            <div className="list__inner">
+          : props.item === 'Units' && isListLoading === true ? <div className="list__inner">
+            <img src={require("../images/loading.gif")} alt="" className="list__loading-img" />
+          </div>
+            : <></>
+        }
+        {props.item === 'CommandCards' && isListLoading === false ? <>
+          <div className="list__inner">
+            <div className="list__exp-num">
+              №
+            </div>
+            <div className="list__leader">
+              Leader
+            </div>
+            <div className="list__item">
+              Name
+            </div>
+          </div>
+          {list.map((el, key) =>
+            <div className="list__inner" key={key}>
               <div className="list__exp-num">
-                №
+                {el.expNum}
               </div>
               <div className="list__leader">
-                Leader
+                {el.leader}
               </div>
-              <div className="list__item">
-                Name
+              <div className="list__item" key={key}>
+                <a href={el.link} className="list__link">
+                  {el.name}
+                </a>
               </div>
             </div>
-            {list.map((el, key) =>
-              <div className="list__inner" key={key}>
-                <div className="list__exp-num">
-                  {el.expNum}
-                </div>
-                <div className="list__leader">
-                  {el.leader}
-                </div>
-                <div className="list__item" key={key}>
-                  <a href={el.link} className="list__link">
-                    {el.name}
-                  </a>
-                </div>
+          )}</>
+          : props.item === 'CommandCards' && isListLoading === true ? <div className="list__inner">
+            <img src={require("../images/loading.gif")} alt="" className="list__loading-img" />
+          </div>
+            : <></>
+        }
+        {props.item === 'Upgrades' && isListLoading === false ? <>
+          <div className="list__inner">
+            <div className="list__date">
+              Date
+            </div>
+            <div className="list__type">
+              Type
+            </div>
+            <div className="list__item">
+              Name
+            </div>
+            <div className="list__restrictions">
+              Restrictions
+            </div>
+          </div>
+          {list.map((el, key) =>
+            <div className="list__inner" key={key}>
+              <div className="list__date">
+                {el.date}
               </div>
-            )} </>
-            : <>
-              <div className="list__inner">
-                <div className="list__date">
-                  Date
-                </div>
-                <div className="list__type">
-                  Type
-                </div>
-                <div className="list__item">
-                  Name
-                </div>
-                <div className="list__restrictions">
-                  Restrictions
-                </div>
+              <div className="list__type">
+                {el.type}
+                {/* <img
+                       src={require(`../images/${el.type}.webp`)}
+                        src={`../images/${el.type}.webp`}
+                       alt="shit"
+                       height="20"
+                       width="20"
+                     /> */}
               </div>
-              {list.map((el, key) =>
-                <div className="list__inner" key={key}>
-                  <div className="list__date">
-                    {el.date}
-                  </div>
-                  <div className="list__type">
-                    {el.type}
-                  </div>
-                  <div className="list__item" key={key}>
-                    <a href={el.link} className="list__link">
-                      {el.name}
-                    </a>
-                  </div>
-                  <div className="list__restrictions">
-                    {el.restrictions}
-                  </div>
-                </div>
-              )} </>}
-
+              <div className="list__item" key={key}>
+                <a href={el.link} className="list__link">
+                  {el.name}
+                </a>
+              </div>
+              <div className="list__restrictions">
+                {el.restrictions}
+              </div>
+            </div>
+          )} </>
+          : props.item === 'Upgrades' && isListLoading === true ? <div className="list__inner">
+            <img src={require("../images/loading.gif")} alt="" className="list__loading-img" />
+          </div>
+            : <></>}
 
 
 
